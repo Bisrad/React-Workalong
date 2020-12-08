cf85555d9b40d97f9659b446298fb276e723c362
Brad Thompson <bthompson6225@gmail.com>
2020-12-08 15:51:01 -0700

    Add CSSTransitions to Order.js

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -7,20 +7,47 @@ class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
+    const transitionOptions = {
+      classNames: "order",
+      key,
+      timeout: { enter:250, exit:250 }
+    };
     // Make sure the fish is loaded before continuing 
     if (!fish) return null; 
 
     if (!isAvailable) {
-      return <li key={key}>Sorry {fish ? fish.name: 'fish'} is no longer available</li>
+      return (
+        <CSSTransition {...transitionOptions}>
+          <li key={key}>
+            Sorry {fish ? fish.name: 'fish'} is no longer available
+          </li>
+        </CSSTransition>
+      );
     }
     return (
-      <li key={key}>
-        {count} lbs {fish.name}
-        {formatPrice(count * fish.price)}
-        <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
-      </li>
-      );
-    };
+      <CSSTransition {...transitionOptions}>
+        <li key={key}>
+          <span>
+            <TransitionGroup component="span" className="count">
+              <CSSTransition
+                classNames="count"
+                key={count}
+                timeout={{ enter: 500, exit: 500 }}
+              >
+                <span className="count">{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
+            <strong>lbs </strong>
+            <span className="name">{fish.name}</span>
+            {formatPrice(count * fish.price)}
+          </span>
+            <button className="remove" onClick={() => this.props.removeFromOrder(key)}>
+              &times;
+            </button>
+        </li>
+      </CSSTransition>
+    );
+  };
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
