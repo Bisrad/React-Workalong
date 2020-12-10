e6da482c718c2773d5a4d86d1f74b33382870d71
Brad Thompson <bthompson6225@gmail.com>
2020-12-09 21:17:41 -0700

    Updated hidden keys

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase';
 
 // Firebase App
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDICF1-JANHTA2Brd_Shpg0wP5eK1vLE3Y",
-    authDomain: "catch-of-the-day-brad-thompson.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-brad-thompson-default-rtdb.firebaseio.com",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
   });
 
 // Rebase Binding
